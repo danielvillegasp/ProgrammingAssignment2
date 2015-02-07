5e483919aaf1fa34e786901e58496ee5b4c9fc2f
Daniel <daniel.villegas.p@gmail.com>
2015-02-07 17:21:05 -0500

    Change indentation

--- cachematrix.R
+++ cachematrix.R
@@ -15,19 +15,22 @@
 ## ...$getinverse() returns the matrix stored by setinverse
 
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
-  set <- function(y){
-    x<<-y
-    inv <<- NULL
-  }
-  get <- function() x
-  setinverse <- function(inverse) {
-    if(!identical(dim(inverse), dim(x)))
-      warning("The given inverse dimentions does not match the stored matrix dimentions.")
-    inv <<-inverse
-  }
-  getinverse <- function() inv
-  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+    inv <- NULL
+    set <- function(y){
+        x<<-y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) {
+        if(!identical(dim(inverse), dim(x)))
+            warning("The given inverse dimentions does not match the stored matrix dimentions.")
+        inv <<-inverse
+    }
+    getinverse <- function() inv
+    list(set=set, 
+         get=get, 
+         setinverse=setinverse, 
+         getinverse=getinverse)
 }
 
 
@@ -35,14 +38,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ## This function takes an argument X and optional arguments that are passed to solve as solve(X, ...).
 ## Then it checks whether the inverse exists, if it does it returns the inverse if it doesn't it 
 ## calculates it and then stores it in the passed object.
-##  ...$get() returns X ^ -1
-##  ...$getinverse() returns X 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-  if(!is.null(x$getinverse())){
-    x$getinverse()
-  }else{
-    x$setinverse(solve(x$get()), ...) 
-  }
+    ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getinverse() 
+    if(!is.null(inverse)){
+        inverse
+    }else{
+        x$setinverse(solve(x$get()), ...) 
+    }
 }
